632ec79eb6f3f34c114862ee333347a761a49dcb
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2025-02-21 22:38:21 +0700

    Add Exception Handler Message for Validation failed

--- epl-web/src/main/java/vn/ptit/project/epl_web/util/exception/GlobalException.java
+++ epl-web/src/main/java/vn/ptit/project/epl_web/util/exception/GlobalException.java
@@ -2,9 +2,19 @@ package vn.ptit.project.epl_web.util.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import vn.ptit.project.epl_web.dto.response.RestResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalException {
@@ -17,8 +27,10 @@ public class GlobalException {
         response.setMessage(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
+
     @ExceptionHandler(value = {
-            InvalidRequestException.class
+            InvalidRequestException.class,
+            UsernameNotFoundException.class
     } )
     public ResponseEntity<RestResponse<Object>> handleInvalidRequestException(InvalidRequestException exception) {
         RestResponse<Object> response = new RestResponse<>();
@@ -27,4 +39,29 @@ public class GlobalException {
         response.setError("Exception occurs");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
+    // Bắt exception validation
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RestResponse<Object>> validationError(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        final List<FieldError> fieldErrors = result.getFieldErrors();
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(ex.getBody().getDetail());
+
+        // Viết bằng cú pháp lamda
+        // List<String> errors = fieldErrors.stream().map(f ->
+        // f.getDefaultMessage()).collect(Collectors.toList());
+        // res.setMessage(errors.size() > 1 ? errors : errors.get(0));
+
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : fieldErrors) {
+            errors.add(error.getDefaultMessage());
+        }
+        res.setMessage(errors.size() > 1 ? errors : errors.get(0));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+
+    }
+
 }
